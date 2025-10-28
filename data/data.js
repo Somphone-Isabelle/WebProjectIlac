a58d7a62e0e462bca1536cf618b6020140df222a
Somphone-Isabelle <isabelle.somphone@efrei.net>
2025-10-28 10:37:04 -0400

    first

---
+++ data/data.js
@@ -0,0 +1,18 @@
+
+
+const data = [{
+    title: "Aventurine",
+    desc: "Aventurine is a character in the game HSR, known for being a senior manager in the IPC strategic investment department.",
+    img: "https://tse1.mm.bing.net/th/id/OIP.-sxK_LUwF1wCLZXJtoTPtAHaEK?rs=1&pid=ImgDetMain&o=7&rm=3"
+},
+{
+    title: "Luocha",
+    desc: "Luocha is a character from the game HSR, a foreign trader with medical skills",
+    img: "https://www.pockettactics.com/wp-content/sites/pockettactics/2023/06/honkai-star-rail-luocha-5.jpg"
+},
+{
+    title: "Mydei",
+    desc: "Mydei is a character from the game HSR, depicted as a royal figure with a tragic past",
+    img: "https://cdn.oneesports.gg/cdn-data/2024/12/HonkaiStarRail_Mydei_Wallpaper_CloseUp2.jpeg"
+}
+]; 
